f7558060f76fa11426d5d8e05b07c4f6e7bee631
yilin@workstation <linda.yilin@gmail.com>
2017-10-04 03:22:41 +0800

    validation

--- hw1/src/train.py
+++ hw1/src/train.py
@@ -11,30 +11,31 @@ def process_data(train_path, test_path):
           .apply(lambda s:pd.to_numeric(s, errors='conerce')).fillna(0).groupby('日期')
           .apply(lambda df: df.reset_index(['日期', '測站'], drop=True).T))
 
-    '''
     train_df = df[(df.index.get_level_values('日期').day < 17) | (df.index.get_level_values('日期').day > 20)]
     vali_df = df[(df.index.get_level_values('日期').day >= 17) & (df.index.get_level_values('日期').day <= 20)]
     '''
     train_df = df
     vali_df = None
+    '''
 
     test_df = (pd.read_csv(test_path, header=None, index_col=[0, 1])
                .apply(lambda s:pd.to_numeric(s, errors='conerce')).fillna(0).groupby(0)
                .apply(lambda df: df.reset_index([0], drop=True).T))
 
-    train_df_scaled = (train_df - train_df.mean()) / train_df.std()
-    '''
-    vali_df_scaled = (vali_df - train_df.mean()) / train_df.std()
+    train_df_scaled = (train_df - df.mean()) / df.std()
+    vali_df_scaled = (vali_df - df.mean()) / df.std()
     '''
     vali_df_scaled = None
-    test_df_scaled = (test_df - train_df.mean()) / train_df.std()
+    '''
+    test_df_scaled = (test_df - df.mean()) / df.std()
 
     return train_df, vali_df, test_df, train_df_scaled, vali_df_scaled, test_df_scaled
 
 def df2feature(df, df_scaled):
     x = []
     y = []
-    for i in range(0, df.shape[0]-10, 10):
+    for i in range(0, df.shape[0]-10):
+        # overlap
         tmpdf = df[i:i+10]
         tmpdf_scaled = df_scaled[i:i+10]
         date = tmpdf.index.get_level_values('日期')
@@ -42,7 +43,7 @@ def df2feature(df, df_scaled):
             # only PM2.5
             #x.append(tmpdf['PM2.5'].values[:-1])
             # PM2.5 O3
-            x.append(tmpdf_scaled[['PM2.5', 'PM10', 'NO2']].values[:-1].flatten())
+            x.append(tmpdf_scaled[['PM2.5', 'PM10', 'NO2', 'NOx', 'SO2', 'AMB_TEMP', 'O3', 'RH']].values[:-1].flatten())
             # all
             #x.append(tmpdf_scaled.values[:-1].flatten())
             #x.append(tmpdf.values[:-1].flatten())
@@ -55,7 +56,7 @@ def testdf2feature(df):
     # only pm2.5
     #return df.groupby(level=[0]).apply(lambda df: df['PM2.5'].reset_index(0, drop=True))
     # PM2.5 O3
-    return df.groupby(level=[0]).apply(lambda df: df[['PM2.5', 'PM10', 'NO2']].stack().reset_index(0, drop=True))
+    return df.groupby(level=[0]).apply(lambda df: df[['PM2.5', 'PM10', 'NO2', 'NOx', 'SO2', 'AMB_TEMP', 'O3', 'RH']].stack().reset_index(0, drop=True))
     # all
     #return df.groupby(level=[0]).apply(lambda df: df.stack().reset_index(0, drop=True))
 
@@ -75,7 +76,9 @@ def evaluation(y_pred, y):
 def main(args):
     train_df, vali_df, test_df, train_df_scaled, vali_df_scaled, test_df_scaled = process_data(args.train_path, args.test_path)
     train_x, train_y = df2feature(train_df, train_df_scaled)
-    #vali_x, vali_y = df2feature(vali_df, vali_df_scaled)
+    vali_x, vali_y = df2feature(vali_df, vali_df_scaled)
+    x = np.append(train_x, vali_x, axis=0)
+    y = np.append(train_y, vali_y)
 
     #feature_df = testdf2feature(test_df)
     feature_df = testdf2feature(test_df_scaled)
@@ -88,18 +91,21 @@ def main(args):
     w = np.ones(len(train_x[0])) / len(train_x[0])
 
     for t in range(args.epoch):
+        '''
         vali_score = 0
         for train_index, vali_index in kf.split(train_x):
             tmp_w = gdfit(train_x[train_index], train_y[train_index], w, args.lr)
             vali_score += evaluation(predict(tmp_w, train_x[vali_index]), train_y[vali_index])
+        '''
+        tmp_w = gdfit(train_x, train_y, w, args.lr)
+        vali_rmse = evaluation(predict(tmp_w, vali_x), vali_y)
 
-        w = gdfit(train_x, train_y, w, args.lr)
+        w = gdfit(x, y, w, args.lr)
         w.dump(f'result/model/{args.prefix}_{args.lr}_{t}') 
 
         feature_df['value'] = predict(w, test_x)
         feature_df['value'].to_csv(f'result/prediction/{args.prefix}_{args.lr}_{t}', header=True)
-        train_rmse = evaluation(predict(w, train_x), train_y)
-        vali_rmse = vali_score / 3
+        train_rmse = evaluation(predict(w, x), y)
         print(f'{t},{train_rmse:.8f},{vali_rmse}')
         fp.write(f'{t},{train_rmse},{vali_rmse}\n')
 
